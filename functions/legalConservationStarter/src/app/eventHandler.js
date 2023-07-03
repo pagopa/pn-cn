f78c8536eb2691249fcada6203e3128562fc516f
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-07-03 18:27:10 +0200

    Added legal conservation starter to log events

--- functions/legalConservationStarter/src/app/eventHandler.js
+++ functions/legalConservationStarter/src/app/eventHandler.js
@@ -1,3 +1,45 @@
+const { extractKinesisData } = require('./kinesis')
+
 exports.handleEvent = async (event) => {
-    throw new Error("Not implemented")
+
+  console.log("event: ", event);
+
+  // basic return payload
+  const payload = {
+    batchItemFailures: [],
+  };
+
+  let processedItems = [];
+
+
+  // "normal" Kinesis path
+  console.log("*** Kinesis processing ***");
+
+  // 1. get event from Kinesis and filter for delete
+  const kinesisEvents = extractKinesisData(event);
+  console.log(JSON.stringify(kinesisEvents))
+
+  throw new Error("Not implemented")
+
+  console.log(`Kinesis items to persist`, processedItems);
+
+  // 2. process if reason is TTL: create an Active SLA Violation (part common to Kinesis and SQS path)
+  const persistSummary = await processEvents(processedItems); // actually produce changes to DB (in our case create Active Sla Violations or storicize them)
+
+  console.log("Persist summary", persistSummary);
+  console.log(`Inserted ${persistSummary.insertions} records`);
+  console.log(`Updated ${persistSummary.updates} records`);
+
+  if (persistSummary.errors.length > 0) {
+    console.error(
+      `Legal Conservation Starter execution finished with ${persistSummary.errors.length} errors`,
+      persistSummary.errors
+    );
+    payload.batchItemFailures = persistSummary.errors.map((i) => {
+      return { itemIdentifier: i.kinesisSeqNumber }; 
+    });
+  }
+
+  return payload;
+
 };
