fb3f699b4ec43b79cf79b604ce52d9da7dfddf98
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-07-05 09:38:30 +0200

    Added event validation

--- functions/eventUpdateReceiver/src/app/eventHandler.js
+++ functions/eventUpdateReceiver/src/app/eventHandler.js
@@ -1,13 +1,18 @@
-const { validateRequest, respondError, respondOk, getEventsFromBody } = require('./requestHelper')
+const { validateRequest, validateEvents, respondError, respondOk, getEventsFromBody } = require('./requestHelper')
 const { processEventUpdate } = require('./eventProcessor')  
 
 exports.handleEvent = async (event) => {
     console.log('event', event)
     const isRequestValid = validateRequest(event)
     if(!isRequestValid){
-        return respondError({ resultCode: '400.00', resultDescription: 'Invalid request', errorList: [] }, 400, {})
+        return respondError({ resultCode: '404.00', resultDescription: 'Not found', errorList: [] }, 404, {})
     }
     
+    const eventValidationErrors = validateEvents(event.body)
+    if(eventValidationErrors.length>0){
+        return respondError({ resultCode: '400.00', resultDescription: 'Validation error', errorList: eventValidationErrors }, 400, {})
+    }
+
     const events = getEventsFromBody(event.body)
     for(let i=0; i<events.length; i++){
         await processEventUpdate(events[i])
--- functions/eventUpdateReceiver/src/app/requestHelper.js
+++ functions/eventUpdateReceiver/src/app/requestHelper.js
@@ -1,11 +1,15 @@
 exports.validateRequest = function(event){
     const { path, httpMethod, body } = event
     
+    const errors = []
     if(httpMethod==='POST' && path && path==='/cn/v1/events' && body){
         return true
+        const jsonBody = JSON.parse(body)
+        if(jsonBody.events){}
     }
     
-    return false
+    errors.push('Invalid path/method')
+    return errors
 }
 
 exports.respondError = function(errorDetails, statusCode, headers){
@@ -28,4 +32,41 @@ exports.respondOk = function(data, statusCode, headers){
 exports.getEventsFromBody = function(body){
     const jsonBody = JSON.parse(body)
     return jsonBody.events || []
+}
+
+function validateEvent(event){
+    const errors = []
+    if(!event.externalId){
+        errors.push('Missing "externalId"')
+    }
+
+    if(!event.fileKey){
+        errors.push('Missing "externalId"')
+    }
+
+    if(!event.status){
+        errors.push('Missing "externalId"')
+    }
+
+    return errors
+}
+
+exports.validateEvents = function(body){
+    const jsonBody = JSON.parse(body)
+
+    const errors = []
+
+    if(!jsonBody.events){
+        errors.push('Missing "events" key')
+        return errors
+    }
+
+    for(let i=0; i<jsonBody.events.length; i++){
+        const eventErrors = validateEvent(jsonBody.events[i])
+        if(eventErrors.length>0){
+            errors.push('Error for event at index '+i+': '+eventErrors.join(', '))
+        }
+    }
+
+    return errors
 }
