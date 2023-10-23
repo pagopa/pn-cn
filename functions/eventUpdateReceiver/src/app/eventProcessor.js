cfbdc3b4ddf10dc4744e753a6b89d96a8ffff038
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-10-23 09:28:19 +0200

    PN-8557: retry based on error codes

--- functions/eventUpdateReceiver/src/app/eventProcessor.js
+++ functions/eventUpdateReceiver/src/app/eventProcessor.js
@@ -5,8 +5,18 @@ function isError(event){
     return event.status!=='OK'
 }
 
+function getRetryErrorCodes(){
+    const errorCodesConfig = process.env.RETRY_ERROR_CODES
+    if(!errorCodesConfig){
+        return []
+    }
+
+    const errorCodes = errorCodesConfig.split(',').map(i => i.trim())
+    return errorCodes
+}
 function isRetryError(event){
-    return false; // TODO: identify errorCodes that triggers a retry
+    const retryErrorCodes = getRetryErrorCodes()
+    return retryErrorCodes.indexOf(event.errorCode)>=0 
 }
 
 exports.processEventUpdate = async function(event){
@@ -17,12 +27,15 @@ exports.processEventUpdate = async function(event){
     if(isErrorResponse){
         if(isRetryError(event)){
             // refresh ttl to retry
+            console.log('File key '+event.fileKey +' / '+event.externalId+' received KO status with retriable error code '+event.errorCode)
             await ttlRepository.refreshRequestTTL(event)
         } else {
             // delete ttl entity
+            console.log('File key '+event.fileKey +' / '+event.externalId+' received KO status with not retriable error code '+event.errorCode)
             await ttlRepository.deleteRequestTTL(event)
         }
     } else {
+        console.log('File key '+event.fileKey +' / '+event.externalId+' received OK status')
         await ttlRepository.deleteRequestTTL(event)
         // put Record into Kinesis
         await putEventUpdate(event)
