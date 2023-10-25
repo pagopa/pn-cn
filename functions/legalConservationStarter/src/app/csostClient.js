ab69ee1aaa00d6898e85895e9e8a3632be87615d
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-10-25 12:56:23 +0200

    PN-8577: fix logging

--- functions/legalConservationStarter/src/app/csostClient.js
+++ functions/legalConservationStarter/src/app/csostClient.js
@@ -1,25 +1,7 @@
 const { retryHandler } = require('legal-conservation-commons')
 
 // HTTP client for CSOST service (the legal conservation service)
-async function internalIngestDocument(payload, secrets){
-  const url = process.env.CONSERVATION_SERVICE_BASE_URL+'/api/v1/uploads/remote'
-
-  const headers = {
-    'x-api-key': secrets.apiKey,
-    'Content-Type': 'application/json'
-  }  
-  
-  const fetchOptions = { 
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(payload)
-  }
-
-  console.log('[DOWNSTREAM] Invoking external service CSost Ingestion. Waiting Sync response.', {
-    url: url,
-    fetchOptions: fetchOptions
-  })
-
+async function internalIngestDocument(url, fetchOptions){
   const res = await fetch(url, fetchOptions);
     
   const data = await res.json()   
@@ -39,5 +21,23 @@ async function internalIngestDocument(payload, secrets){
 
 // reuse retryHandler to do internalIngestDocument calls (3 retryes with 1000ms delay)
 exports.ingestDocument = async function ingestDocument(payload, secrets){
-  return retryHandler(() => internalIngestDocument(payload, secrets), 3, 1000)
+  const url = process.env.CONSERVATION_SERVICE_BASE_URL+'/api/v1/uploads/remote'
+
+  const headers = {
+    'x-api-key': secrets.apiKey,
+    'Content-Type': 'application/json'
+  }  
+  
+  const fetchOptions = { 
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(payload)
+  }
+
+  console.log('[DOWNSTREAM] Invoking external service CSost Ingestion. Waiting Sync response.', {
+    url: url,
+    fetchOptions: fetchOptions
+  })
+
+  return retryHandler(() => internalIngestDocument(url, fetchOptions), 3, 1000)
 }
